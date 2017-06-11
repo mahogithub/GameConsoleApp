b63a552bc3016a1615c08a0a97a6617efc0a05c2
Maksym <maksymhovorun@gmail.com>
2017-06-11 23:04:23 +0300

    Created MainClass, gitignore

---
+++ src/java/com/qatestlab/MainClass.java
@@ -0,0 +1,14 @@
+package java.com.qatestlab;
+
+/**
+ * Created by user on 11.06.2017.
+ */
+public class MainClass {
+    /**
+     * To run the game
+     * @param args - console arguments to the program
+     */
+    public static void main(String args[]) {
+
+    }
+}

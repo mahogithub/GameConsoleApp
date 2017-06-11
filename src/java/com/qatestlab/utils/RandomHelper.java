e967dd9a20775c147da16bff5ec221b09ad9a0d6
Maksym <maksymhovorun@gmail.com>
2017-06-11 23:09:39 +0300

    Created RandomHelper.java

---
+++ src/java/com/qatestlab/utils/RandomHelper.java
@@ -0,0 +1,26 @@
+package java.com.qatestlab.utils;
+
+import java.com.qatestlab.units.Squad;
+import java.com.qatestlab.units.Unit;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by user on 08.06.2017.
+ * This is class for complex random actions
+ */
+public class RandomHelper {
+    /**
+     * To get random unit of the squad using both privileged and general group
+     * @param squadToGet - squad of the units
+     * @return random unit of the squad
+     */
+    public static Unit getRandomUnit(Squad squadToGet) {
+        ArrayList<Unit> units = new ArrayList<>();
+        units.addAll(squadToGet.getUnits());
+        units.addAll(squadToGet.getPrivilegedUnits());
+        return units.get(new Random().nextInt(units.size()));
+
+    }
+}
